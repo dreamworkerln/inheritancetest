611a1776f4f02d6b90f78d9439b9cd72e253e0e0
hatuncev <hatuncev.ku@kvant-telecom.ru>
2023-12-19 16:58:46 +0300

    added tests

--- src/test/java/ru/home/inheritancetest/services/plants/HiTechPlantTest.java
+++ src/test/java/ru/home/inheritancetest/services/plants/HiTechPlantTest.java
@@ -1,11 +1,16 @@
 package ru.home.inheritancetest.services.plants;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.home.inheritancetest.entities._base.Vehicle;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class HiTechPlantTest {
 
     @Autowired
@@ -13,7 +18,7 @@ class HiTechPlantTest {
 
     @Test
     void produce() {
-
         Vehicle vehicle = hiTechPlant.produce();
+        vehicle.attack(new Object());
     }
 }
